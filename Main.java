45f7ca00befa325a6a0ca9f99d5b3218f5b8aa4c
sujithgunna2005@gmail.com <sujithgunna2005@gmail.com>
2025-07-29 04:18:39 +0000

    Iterator files

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> al=new ArrayList<>();
+        al.add(1);
+        al.add(2);
+        al.add(6);
+        al.add(4);
+        System.err.println("forward");
+        Iterator<Integer> it=al.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+        System.err.println("backward");
+        ListIterator<Integer> i=al.listIterator(al.size());
+        while(i.hasPrevious()){
+            System.out.println(i.previous());
+        }
+
+    }
+}
